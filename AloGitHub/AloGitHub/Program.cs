5d57c5f564b7dc6274caa274809372281de9ab9c
luiz marcello <luizmarcello.vmo@hotmail.com>
2020-06-06 09:16:29 -0300

    Alterei a função escrevenatela

--- AloGitHub/AloGitHub/Program.cs
+++ AloGitHub/AloGitHub/Program.cs
@@ -4,14 +4,14 @@ namespace AloGitHub
 {
     class Program
     { 
-        static void Escrevenatela()
+        static void Escrevenatela(string txt)
         {
-            Console.WriteLine("Segundo teste com o github");
+            Console.WriteLine(txt);
         }
 
         static void Main(string[] args)
         {
-            Escrevenatela();
+            Escrevenatela("Minha terceira alteração no programa");
             Console.ReadKey();
         }
     }
